006194558c07e77508b6409c869fa204ba80945f
ADITYA <adityakumar9873@gmail.com>
2023-03-31 11:25:14 +0530

    async js concept

---
+++ app-2.js
@@ -0,0 +1,19 @@
+// task :-make soup ,
+// boil water 10 min ,
+// chop carrots,
+//  add carrots boil for 5 min ,SetTimer etc
+//  ,chop onions ,
+//  add onions boil for 5 min
+// Browser !!!!! Fetch Data , get Geolocation,setTimeOut,setTimer etc
+
+boilWater(10000);
+console.log(`chop carrots`);
+
+function boilWater(time) {
+  console.log(`boiling...`);
+  // setting setTimeout function
+  setTimeout(() => {
+    ///////this is syntax of setTimeout
+    console.log(`done`);
+  }, time);
+}
